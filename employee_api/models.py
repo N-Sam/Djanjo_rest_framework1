25469664c6ca7ef2a4860ed648f956b195e706ae
N-Sam <nyekosamueltreg@gmail.com>
2020-11-13 13:56:40 +0300

    Feat: created more features

--- employee_api/models.py
+++ employee_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+    emp_code = models.CharField(max_length=3)
+    phone = models.CharField(max_length=15)
